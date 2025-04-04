93aec8ac5d78821a2e155ea12de2e43eb84f19a9
raphael.kieling <raphael.kieling-ext@ab-inbev.com>
2025-04-04 00:44:31 -0300

    fix: removing tests from the output to avoid send it in the release

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'dist'],
+    ignores: ['eslint.config.mjs', 'dist', 'tests/**/*'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
